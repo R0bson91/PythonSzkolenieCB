46def62ab0df931725bf53b26fa655b812319385
Robert Kowalski <robertkowalski91@gmail.com>
2022-02-02 20:56:41 +0100

    Added file

--- Szkolenie.py
+++ Szkolenie.py
@@ -1755,6 +1755,14 @@
 # ---------------------------------------------------------------------------------
 # CWICZENIE
 #
-
-
-
+# class Vehicle:
+#     def __init__(self, name, mileage, capacity):
+#         self.name = name
+#         self.mileage = mileage
+#         self.capacity = capacity
+#
+# class Bus(Vehicle):
+#     pass
+#
+# School_bus = Bus("Szkolne Volvo", 12, 50)
+# print(type(School_bus))
